7db8073e5f98e2bd29d88f64df6d83d0becc301e
jackson.torres.1000 <jackson.torres.1000@gmail.com>
2023-04-30 17:15:45 -0300

    Tratando error

--- js/firebase.js
+++ js/firebase.js
@@ -27,26 +27,30 @@ function limparFormulario() {
 }
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const nome = document.getElementById('nome').value;
-    const assunto = document.getElementById('assunto').value;
-    const mensagem = document.getElementById('mensagem').value;
-    const emailsRef = db.collection('Emails');
-    if(email === '' || nome === '' || assunto === '' || mensagem === '') {
-        alert('Preencha todos os campos!');
-        return;
+    try {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const nome = document.getElementById('nome').value;
+        const assunto = document.getElementById('assunto').value;
+        const mensagem = document.getElementById('mensagem').value;
+        const emailsRef = db.collection('Emails');
+        if (email === '' || nome === '' || assunto === '' || mensagem === '') {
+            alert('Preencha todos os campos!');
+            return;
+        }
+        emailsRef.doc(email).set({
+            email: email,
+            nome: nome,
+            assunto: assunto,
+            mensagem: mensagem,
+            data: new Date()
+        }).then(() => {
+            limparFormulario();
+            alert('Mensagem enviada com sucesso!');
+        }).catch(error => {
+            console.error('Erro ao enviar mensagem:', error);
+        });
+    } catch ( error ) {
+        alert('Erro ao enviar mensagem!', error);
     }
-    emailsRef.doc(email).set({
-        email: email,
-        nome: nome,
-        assunto: assunto,
-        mensagem: mensagem,
-        data: new Date()
-    }).then(() => {
-        limparFormulario();
-        alert('Mensagem enviada com sucesso!');
-    }).catch(error => {
-        console.error('Erro ao enviar mensagem:', error);
-    });
 });
